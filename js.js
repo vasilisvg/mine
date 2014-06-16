ff5795c1c12f78f357fb629072c2ac445169d73e
Vasilis van Gemert <github@vasilis.nl>
2014-06-16 22:55:42 +0200

    Looks good now

--- js.js
+++ js.js
@@ -1,26 +1,23 @@
 // About the list
 
 var theUL = document.querySelector('ul');
-console.log (localStorage.getItem('storedList'));
 if(localStorage.getItem('storedList')) {
     theUL.innerHTML = localStorage.getItem('storedList');
 }
 else {
-    theUL.innerHTML = '<li>…</li>';
+    theUL.innerHTML = '<li></li>';
 }
 var theList = theUL.innerHTML;
 //alert(theList);
 theUL.onkeyup = function() {
-    theList = theUL.innerHTML;
-    localStorage.setItem('storedList', theList);
-    localStorage.getItem('storedList');
-    console.log(localStorage.getItem('storedList'));
+    setLocalStorage();
 }
 
 
 // onclick
 
-document.addEventListener( 'click', clickHandler );
+//document.addEventListener( 'click', clickHandler );
+document.addEventListener( 'touchstart', clickHandler );
 function clickHandler( event ) {
     $this = event.target;
 
@@ -28,29 +25,36 @@ function clickHandler( event ) {
     if($this.nodeName == 'BUTTON'){
         if($this.getAttribute('data-type') == 'shop') {
             theUL.removeAttribute('contenteditable');
-            setState($this,'edit')
+            setState($this,'edit');
         }
         else if($this.getAttribute('data-type') == 'edit') {
             theUL.setAttribute('contenteditable','');
-            setState($this,'shop')
+            setState($this,'shop');
+        }
+        else if($this.getAttribute('data-type') == 'delete') {
+            deleteDeleted();
         }
     }
+
+    // The Del
     else if($this.nodeName == 'DEL'){
-        if($this.parentNode.parentNode.getAttribute('contenteditable') != null) {
+        if($this.parentNode.parentNode.hasAttribute('contenteditable')) {
             return;
         }
         else {
             var listItem = $this.innerHTML;
             $this.parentNode.innerHTML = listItem;
+            setLocalStorage();
         }
     }
     else if($this.nodeName == 'LI'){
-        if($this.parentNode.getAttribute('contenteditable') != null) {
+        if($this.parentNode.hasAttribute('contenteditable')) {
             return;
         }
         else {
             var listItem = $this.innerHTML;
             $this.innerHTML = '<del>' + listItem + '</del>';
+            setLocalStorage();
         }
     }
 }
@@ -60,4 +64,17 @@ function clickHandler( event ) {
 function setState($this,stateType) {
     $this.setAttribute('data-type',stateType);
     $this.innerHTML = stateType;
+}
+function deleteDeleted() {
+    dels = document.querySelectorAll('del');
+    for(var i=0; i<dels.length; i++) {
+        toDelete = dels[i].parentNode;
+        dels[i].parentNode.parentNode.removeChild(toDelete);
+    }
+    setLocalStorage();
+}
+function setLocalStorage() {
+    theList = theUL.innerHTML;
+    localStorage.setItem('storedList', theList);
+    localStorage.getItem('storedList');
 }
